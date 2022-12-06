1e4a95ef3c08d6ea9a8e9f009b7355651e660053
EC2 Default User <ec2-user@ip-172-31-39-31.ap-northeast-1.compute.internal>
2022-12-06 12:05:31 +0000

    マイグレーションファイルのデフォルト値変更

--- db/migrate/20221027065017_create_posts.rb
+++ db/migrate/20221027065017_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :user_id, null: false, default: ""
-      t.integer :item_id, null: false, default: ""
+      t.integer :user_id, null: false, default: 0
+      t.integer :item_id, null: false, default: 0
       t.string :comment, null: false, default: ""
       t.integer :star, null: false, default: 0
 
