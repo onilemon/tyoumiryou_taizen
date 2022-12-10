147c4bdc075825cef94c3f3d67a6264e31a0d10a
EC2 Default User <ec2-user@ip-172-31-39-31.ap-northeast-1.compute.internal>
2022-12-10 12:39:41 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Admin.create!(
-   #email: 'a@a',
-   #password: '123456'
-#)
+Admin.create!(
+   email: 'a@a',
+   password: '123456'
+)
 
-# users_data = [
-#   ["ポチ", "a@a", "20", "男性"],
-#   ["ミケ", "s@s", "25", "女性"],
-#   ["dsfv", "a@asdf", "33", "男性"],
-#   ["クロ", "q@q", "35", "秘密"]
-# ]
+users_data = [
+  ["ポチ", "a@a", "20", "男性"],
+  ["ミケ", "s@s", "25", "女性"],
+  ["dsfv", "a@asdf", "33", "男性"],
+  ["クロ", "q@q", "35", "秘密"]
+]
 
 Item.create!(
   [
@@ -77,19 +77,19 @@ Item.create!(
   ]
 )
 
-# users_attributes = users_data.map do |data|
-#   { nickname: data[0],
-#     email: data[1],
-#     password: "password",
-#     birthday: data[2],
-#     sex: data[3]}
-# end
+users_attributes = users_data.map do |data|
+  { nickname: data[0],
+    email: data[1],
+    password: "password",
+    birthday: data[2],
+    sex: data[3]}
+end
 
-# users_attributes.each do |attr|
-#   User.create!(attr)
-# end
+users_attributes.each do |attr|
+  User.create!(attr)
+end
 
 
-#genres = ["塩", "甘味", "酢", "醤油", "味噌", "ドレッシング", "ソース", "辛味", "スパイス"]
+genres = ["塩", "甘味", "酢", "醤油", "味噌", "ドレッシング", "ソース", "辛味", "スパイス"]
 
-#genres.each{ |genre| Genre.create!(name: genre) }
+genres.each{ |genre| Genre.create!(name: genre) }
